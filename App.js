eca3c63f7eea1c79b1ff6a00f04416ab5d728cf2
RickUbb <ricardovalle2000@hotmail.com>
2025-04-15 16:04:57 -0500

    First commit

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={{ uri: "https://rukminim3.flixcart.com/image/850/1000/kuof5ow0/wall-decoration/9/s/i/cartoon-rick-and-morty-rick-sanchez-wallpaper-1-p111-poster-original-imag7qxqh5fefcfy.jpeg?q=90&crop=false" }} style={{ width: 215, height:294 }} />
+      <Text>Rick Sanchez!</Text>
+      <Button title="wubba" onPress={() => alert("Wubba lubba dub dub!")} />
+      <StatusBar style="dark" />
     </View>
   );
 }
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f4e4ae',
     alignItems: 'center',
     justifyContent: 'center',
   },
